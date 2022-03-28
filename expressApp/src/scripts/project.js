bed5d13d10cbf1fe63944f3ece29e86671404f74
Alexandre <chorizosurfista@gmail.com>
2022-03-28 12:01:52 +0200

    page projet bien avancer.

--- expressApp/src/scripts/project.js
+++ expressApp/src/scripts/project.js
@@ -1,13 +1,24 @@
-//alert("ok")
-//console.log(elementCreator());
 const projectObj={
     filesPath: "/home/user/",
     projectPath: "Projects",
-    lateralNavFolder: ["User","Desktop","Documents","Downloads","Project","Components"],
+    componentPath: "Components",
+    componentDisplayed: true,
+    lateralNavFolder: ["User","Desktop","Documents","Downloads","Projects","Components"],
     lateralNavSection: ["Computer","Devices","Networks"],
+    networkDevicesData: [{
+        name: "Hard Drive",
+        imagePath: "http://localhost:4000/src/images/project/hard-drive.svg"
+    },{
+        name: "Browse Networks",
+        imagePath: "http://localhost:4000/src/images/project/network.svg",
+    }],
     componentData: [],
     projectData: [],
-    componentPath: "Components",
+    mainContainer: elementCreator({tag:"div", classList:"project-main-container"}),
+    componentsTab: elementCreator({tag: "div", classList:"component-tab", text: "Components"}),
+    projectTab: elementCreator({tag:"div", classList: "project-tab", text: "Project"}),
+    filesDeco: elementCreator({tag: "div", classList:"files-deco",text:"Files"}),
+    iconsLateralNavContainer: elementCreator({tag:"div", classList:"icons-lateralNav-container"}),
     async componentFetch(){
         await fetch("http://localhost:4000/my-component")
         .then(response=>response.json())
@@ -18,8 +29,12 @@ const projectObj={
         .then(response=>response.json())
         .then(data=>this.projectData=data);
     },
-    createLateralNav(tagId){
-        const targetTag= document.getElementById(tagId);
+    createPathBar(){
+        const pathBar= elementCreator({tag: "div", classList: "path-bar-container", text:  this.componentDisplayed?this.filesPath+this.componentPath:this.filesPath+this.projectPath});
+        this.mainContainer.appendChild(pathBar)
+    },
+    createLateralNav(){
+        const targetTag= this.mainContainer //document.getElementById(tagId);
         const lateralContainer=elementCreator({tag:"aside"});
         this.lateralNavSection.forEach((item,i)=>{
             lateralContainer.append(elementCreator({tag: "div", classList: `${item.toLowerCase()}-container`}));
@@ -29,27 +44,55 @@ const projectObj={
             const folder=elementCreator({tag: "div"});
             folder.append(elementCreator({tag:"img", src:"http://localhost:4000/src/images/project/folder.svg", alt: item}));
             folder.append(elementCreator({tag: "p", text: item}));
+            if(item==="Projects"){
+                folder.classList.add(`${item.toLowerCase()}`)
+                const projectFolder=elementCreator({tag: "div", classList: "project-folder"});
+                this.projectData.map((item,i) => {
+                    console.log("here");
+                    projectFolder.append(elementCreator({tag:"div"}));
+                    projectFolder.children[i].append(elementCreator({tag: "img", src: item.langages_frameworks_image_path, alt: item.langages_frameworks_name}));
+                    projectFolder.children[i].append(elementCreator({tag:"p", text:`${item.project_name}.${item.langages_frameworks_name}`}));
+                });
+                folder.append(projectFolder);
+            };
             lateralContainer.children[0].appendChild(folder);
         });
-        lateralContainer.children[1].append(elementCreator({tag:"div"}));
-        lateralContainer.children[1].children[1].append(elementCreator({tag: "img", src: "http://localhost:4000/src/images/project/hard-drive.svg", alt: "network"}));
-        lateralContainer.children[1].children[1].append(elementCreator({tag: "p", text: "Hard Drive"}));
-//Voir pour une fonction
-        lateralContainer.children[2].append(elementCreator({tag:"div"}));
-        lateralContainer.children[2].children[1].append(elementCreator({tag: "img", src: "http://localhost:4000/src/images/project/network.svg", alt: "hard-drive"}));
-        lateralContainer.children[2].children[1].append(elementCreator({tag: "p", text: "Browse Network"}));
-        targetTag.prepend(lateralContainer);
+        this.networkDevicesData.map((item,i)=>{
+            lateralContainer.children[1+i].append(elementCreator({tag:"div"}));
+            lateralContainer.children[1+i].children[1].append(elementCreator({tag: "img", src: item.imagePath, alt: item.name}));
+            lateralContainer.children[1+i].children[1].append(elementCreator({tag: "p", text: item.name}));
+        });
+        targetTag.children[2].append(lateralContainer);
     },
-    createPathBar(){
-
+    createIcons(iconsData){
+        const iconsContainer= elementCreator({tag: "div", classList:"icons-container"});
+        iconsData.map((item)=>{
+            const icon=elementCreator({tag: "a"});
+            icon.append(elementCreator({tag: "img", src: item.langages_frameworks_image_path, alt:item.component_name}));
+            icon.append(elementCreator({tag:"p", text:`${item.component_name.replace(" ","-").toLowerCase()}.${item.langages_frameworks_name}`}));
+            iconsContainer.append(icon);
+        });
+        this.mainContainer.children[2].append(iconsContainer);
     },
-    createIcons(){
-
+    async generateUi(tagId){
+        const targetTag=document.getElementById(tagId)
+        let sucess=false;
+        do{
+            await this.componentFetch();
+            await this.projectFetch();
+            sucess=true;
+        }while(!sucess){
+            console.log("here");
+            this.createPathBar()
+            this.mainContainer.append(this.filesDeco)
+            this.mainContainer.append(this.iconsLateralNavContainer)
+            this.createLateralNav()
+            this.createIcons(this.componentData)
+            targetTag.prepend(elementCreator({tag:"div", classList:"tab-container"}))
+            targetTag.children[0].append(this.projectTab)
+            targetTag.children[0].prepend(this.componentsTab)
+            targetTag.append(this.mainContainer)
+        };
     },
-    generateUi(tagId){
-        this.componentFetch();
-        this.projectFetch();
-        this.createLateralNav(tagId);
-    }
 };
 projectObj.generateUi("project-main")
